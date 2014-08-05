9da03b6f90de54bedef8e6a0916b3e25c02c9eac
Miguel Grimaldos <m.grimaldosmoreno@cranfield.com>
2014-08-05 02:00:16 +0100

    Server side now can do video conversion from local file

--- src/thesisServer/Server.java
+++ src/thesisServer/Server.java
@@ -3,12 +3,15 @@ package thesisServer;
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import it.sauronsoftware.jave.*;
 
 public class Server {
 
@@ -32,6 +35,8 @@ public class Server {
 				
 				long beginning = System.nanoTime();
 				
+				transcode();
+				
 				dataInputStream = new DataInputStream(socket.getInputStream());
 				dataOutputStream = new DataOutputStream(
 						socket.getOutputStream());
@@ -41,20 +46,7 @@ public class Server {
 				int power = Integer.parseInt(dataInputStream.readUTF());
 				//System.out.println("Base: " + base + " Power: " + power);
 				
-				int n = power;
-				
-				BigInteger resul = new BigInteger("1");
-
-				//System.out.println("Computing...");
-				
-				while (n > 0) {
-					resul = resul.multiply(BigInteger.valueOf(base));
-					--n;
-				}
-				
-				System.out.println(/*"Computed\n" + */"Result = " + resul);
-				
-				dataOutputStream.writeUTF(resul.toString());
+				dataOutputStream.writeUTF(bigPower(base, power).toString());
 				
 				long end = System.nanoTime();
 				
@@ -96,6 +88,49 @@ public class Server {
 					}
 				}
 			}
+		}
+	}
+	
+	public static BigInteger bigPower(long base, int power){
+		
+		int n = power;
+		
+		BigInteger resul = new BigInteger("1");
+		
+		while (n > 0) {
+			resul = resul.multiply(BigInteger.valueOf(base));
+			--n;
+		}
+		
+		System.out.println(/*"Computed\n" + */"Result = " + resul);
+		
+		return resul;
+	}
+	
+	public static void transcode(){
+		File source = new File("source.mp4");
+		File target = new File("target.avi");
+		AudioAttributes audioAttributes = new AudioAttributes();
+		audioAttributes.setCodec("libmp3lame");
+		audioAttributes.setBitRate(new Integer(128000));
+		audioAttributes.setChannels(new Integer(2));
+		audioAttributes.setSamplingRate(new Integer(44100));
+		VideoAttributes videoAttributes = new VideoAttributes();
+		videoAttributes.setCodec("mpeg4");
+		videoAttributes.setBitRate(new Integer(160000));
+		videoAttributes.setFrameRate(new Integer(15));
+		videoAttributes.setSize(new VideoSize(400, 300));
+		EncodingAttributes attributes = new EncodingAttributes();
+		attributes.setFormat("avi");
+		attributes.setAudioAttributes(audioAttributes);
+		attributes.setVideoAttributes(videoAttributes);
+		Encoder encoder = new Encoder();
+		
+		try {
+			encoder.encode(source, target, attributes);
+		} catch (IllegalArgumentException | EncoderException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 	}
 
