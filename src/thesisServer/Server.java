7236c848c1483d711488d103016485c03bf4d374
Miguel Grimaldos <m.grimaldosmoreno@cranfield.com>
2014-08-09 10:30:44 +0100

    Ignoring media files

--- src/thesisServer/Server.java
+++ src/thesisServer/Server.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.net.ServerSocket;
